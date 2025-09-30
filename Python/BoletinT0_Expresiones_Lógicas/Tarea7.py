00c6318d31e4ddd297e887c5ead8dc9ada9a2702
gcoz076 <gcoz076@g.educaand.es>
2025-09-30 09:48:55 +0200

    Subo cambios

--- Python/BoletinT0_Expresiones_Lógicas/Tarea7.py
+++ Python/BoletinT0_Expresiones_Lógicas/Tarea7.py
@@ -1,6 +1,8 @@
 DiasLaburados = int(input("¿Cuantos días laburados llevas? "))
-if DiasLaburados > 364:
+if DiasLaburados > 365:
     print ("Tienes derecho a los 30 días de vacaciones pagadas")
 else:
-    print ("No tienes derecho a las vacaciones pagadas todavía")
+    Diasrestantes = 365 - DiasLaburados
+    print ("No tienes derecho a las vacaciones pagadas todavía, te quedan"
+           , Diasrestantes, "días para las vacaciones")
 print ("Final de la encuesta")
